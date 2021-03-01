58c87a77af4c6d89c9313bc9216e4a4dfaa960d0
Hydro <45236616+0x4Hydro@users.noreply.github.com>
2021-03-01 16:08:06 +0200

    Update MuddyTweak_sample_research.cs

--- MuddyTweak_sample_research.cs
+++ MuddyTweak_sample_research.cs
@@ -65,7 +65,7 @@ DateTime checks_for_today_date = DateTime.Today; // The malware checks the date 
 
 DateTime malware_may_not_work_in_7_days = HVxTSglIFVwtn.AddDays(7); // Then it scheduled the expired days for 7, the malware doesn't seem like it will delete itself, it will probably won't work anymore, see code below for more info. The variable changed so it's easier to see what the code does.
 
-if (checks_for_today_data < HVxTSglIFVwtn ) { // Checks the date of today, and checks how much days left for the malware to be able to be executable
+if (checks_for_today_date < HVxTSglIFVwtn ) { // Checks the date of today, and checks how much days left for the malware to be able to be executable
 
 	if (!System.Diagnostics.Debugger.IsAttached) { // Checks for a debugger
 	
